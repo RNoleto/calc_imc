8b9ca075a44c4957a4fbfbb0c9d3c5ef58d532f1
Ramon Noleto <rnoleto@focosp.com>
2022-02-02 11:49:55 -0300

    first commit

---
+++ imc.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+main() {
+  print("=== Digite seu peso ===");
+  var peso = int.parse(stdin.readLineSync() ?? '0');
+
+  print("=== Digite sua altura ===");
+  var altura = double.parse(stdin.readLineSync() ?? '0');
+
+  var calcImc = peso / (altura * altura);
+
+  print(calcImc);
+
+  print("===================");
+  if (calcImc < 18.5) {
+    print("Abaixo do peso");
+  } else if (calcImc > 18.5 && calcImc < 24.9) {
+    print("Peso normal");
+  } else if (calcImc > 25 && calcImc < 29.9) {
+    print("Sobrepeso");
+  } else if (calcImc > 30 && calcImc < 34.9) {
+    print("Obesidade grau 1");
+  } else if (calcImc > 35 && calcImc < 39.9) {
+    print("Obesidade grau 2");
+  } else {
+    print("Obesidade grau 3");
+  }
+}
