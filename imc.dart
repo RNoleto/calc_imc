2b8cb92d557a6a3b4715c936462825d54f842a5f
Ramon Noleto <rnoleto@focosp.com>
2022-02-02 12:41:37 -0300

    mudança de variaveis

--- imc.dart
+++ imc.dart
@@ -6,27 +6,27 @@ main() {
 
 calculoImc() {
   print("=== Digite seu peso ===");
-  var peso = int.parse(stdin.readLineSync() ?? '0');
+  int peso = int.parse(stdin.readLineSync() ?? '0');
 
   print("=== Digite sua altura ===");
   var altura = double.parse(stdin.readLineSync() ?? '0');
 
-  var calcImc = peso / (altura * altura);
+  var imc = peso / (altura * altura);
 
-  imprimirResultado(calcImc);
+  imprimirResultado(imc);
 }
 
-imprimirResultado(calcImc) {
+imprimirResultado(imc) {
   print("===================");
-  if (calcImc < 18.5) {
+  if (imc < 18.5) {
     print("Abaixo do peso");
-  } else if (calcImc > 18.5 && calcImc < 24.9) {
+  } else if (imc > 18.5 && imc < 24.9) {
     print("Peso normal");
-  } else if (calcImc > 25 && calcImc < 29.9) {
+  } else if (imc > 25 && imc < 29.9) {
     print("Sobrepeso");
-  } else if (calcImc > 30 && calcImc < 34.9) {
+  } else if (imc > 30 && imc < 34.9) {
     print("Obesidade grau 1");
-  } else if (calcImc > 35 && calcImc < 39.9) {
+  } else if (imc > 35 && imc < 39.9) {
     print("Obesidade grau 2");
   } else {
     print("Obesidade grau 3");
